fbb69405ddd0a572cf28a4946cbdcd4994263cb8
Melissa Deven <99757260+Deven1991@users.noreply.github.com>
2022-08-12 13:16:41 -0500

    updated models and routes

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const User = require('./User');
 
-// const Reaction = require('./Reaction');
+// Const Reaction = require('./Reaction');
 const Thought = require('./Thought');
 
 module.exports = { User, Thought };
